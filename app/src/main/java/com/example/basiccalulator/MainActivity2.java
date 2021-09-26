2b1e83de47aa85a53e591f09fccdb5fd585a6064
sarahworthington <smworthingto@wisc.edu>
2021-09-26 16:21:12 -0500

    checkoff

--- app/src/main/java/com/example/basiccalulator/MainActivity2.java
+++ app/src/main/java/com/example/basiccalulator/MainActivity2.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
