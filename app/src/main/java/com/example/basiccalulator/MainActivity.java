4f758820c37b020f3b3683d47423fc11dd6626f5
sarahworthington <smworthingto@wisc.edu>
2021-09-26 15:56:44 -0500

    milestone 2

---
+++ app/src/main/java/com/example/basiccalulator/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.basiccalulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void add(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String int1 = myTextField.getText().toString();
+        int num1 = Integer.parseInt(int1);
+
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        String int2 = myTextField2.getText().toString();
+        int num2 = Integer.parseInt(int2);
+
+        int sum = num1 + num2;
+        goToActitivity2(sum);
+    }
+
+    public void subtract(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String int1 = myTextField.getText().toString();
+        int num1 = Integer.parseInt(int1);
+
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        String int2 = myTextField2.getText().toString();
+        int num2 = Integer.parseInt(int2);
+
+        int diff = num1 - num2;
+        goToActitivity2(diff);
+    }
+
+    public void multiply(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String int1 = myTextField.getText().toString();
+        int num1 = Integer.parseInt(int1);
+
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        String int2 = myTextField2.getText().toString();
+        int num2 = Integer.parseInt(int2);
+
+        int prod = num1 * num2;
+        goToActitivity2(prod);
+    }
+
+    public void divide(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String int1 = myTextField.getText().toString();
+        int num1 = Integer.parseInt(int1);
+
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        String int2 = myTextField2.getText().toString();
+        int num2 = Integer.parseInt(int2);
+
+        int quot = num1 / num2;
+        goToActitivity2(quot);
+    }
+
+    public void goToActitivity2(int value){
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("message", value);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/basiccalulator/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.basiccalulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView2 = (TextView) findViewById(R.id.textView2);
+        Intent intent = getIntent();
+
+        int str = intent.getIntExtra("message", 0); //
+        //Log.d(getClass().getName(), "str = " + str);
+        textView2.setText("" + str);
+    }
+}
